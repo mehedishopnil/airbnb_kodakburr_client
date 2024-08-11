d28ba8c462716c316d1e4d3273443cd6fe234a44
SK Mehedi Hasan <skmehedihasan.jr1@gmail.com>
2024-08-11 23:39:37 +0600

    new update

--- src/providers/AuthProvider/AuthProvider.jsx
+++ src/providers/AuthProvider/AuthProvider.jsx
@@ -23,7 +23,7 @@ const AuthProvider = ({ children }) => {
     const fetchHotelData = async () => {
       setLoading(true);
       try {
-        const response = await fetch('https://airbnb-server-theta.vercel.app/hotelData');
+        const response = await fetch('https://airbnb-darius-palmer-server.vercel.app/hotel-data');
         if (!response.ok) {
           throw new Error(`Error fetching hotelData.json: ${response.status} ${response.statusText}`);
         }
